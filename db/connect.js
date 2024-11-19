dc86b42497f5986a313ee492983384a7b60508f1
Carlos Pinheiro <79546952+CarlosfcPinheiro@users.noreply.github.com>
2024-11-19 23:07:49 +0000

    adicionar dependencias: Sequelize e pg; Adicionar script de conexão com o banco

--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,23 @@
-// host = aws-0-sa-east-1.pooler.supabase.com
-// database port = 6543
-// user = postgres.wotdyyymccicaspuxktw
-// database password = toor!@#123R
+// Connecting database Postgres with ORM Sequelize
+// Importing Sequelize and DataTypes class
+const {Sequelize} = require('sequelize');
+// Creating sequelize instance
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'aws-0-sa-east-1.pooler.supabase.com',
+    username: 'postgres.wotdyyymccicaspuxktw',
+    password: 'toor!@#123R',
+    database: 'postgres',
+    logging: true,
+});
+
+const connectDB = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log('Connected with DataBase.');
+    } catch(err){
+        console.log(`DataBase connection error: ${err}`);
+    }
+}
+
+connectDB();
