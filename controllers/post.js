0d26b5c13b5dfff545a85b4960bc72451c0a9b04
Carlos Pinheiro <79546952+CarlosfcPinheiro@users.noreply.github.com>
2024-11-22 00:33:37 +0000

    adicionar base dos metodos a entidade post

--- controllers/post.js
+++ controllers/post.js
@@ -2,6 +2,7 @@
 const User = require('../models/user');
 // Importing Post model
 const Post = require('../models/post');
+const { where } = require('sequelize');
 
 // Creating post controlelrs
 const getAllPosts = async (req, res) => {
@@ -23,21 +24,42 @@ const getAllPosts = async (req, res) => {
             error: err.message
         });
     }
-
 }
-
+// Get a posts by userId
 const getPost = async (req, res) => {
-    res.send('GET an Specific Post.');
+    const {id} = req.param;
+    try{
+        const posts = await Post.findAll({
+            where: {userid:id}
+        });
+        res.status(200).json(posts);
+    } catch (err){
+        res.status(500).json({
+            message: 'Error retriving post by id'
+        });
+    }
+    // res.send('GET an Specific Post.')
 }
-
+// Create new Post by User id
 const postUserPost = async (req, res) => {
-    res.send('POST an User Post.');
+    const {description, img_data=null, userid} = req.body;
+    try{
+        const newPost = await Post.create({description, img_data, userid});
+        res.send(201).json({
+            message: 'New Post created successfull',
+            post: newPost
+        });
+    } catch (err){
+        res.status(500).json({
+            message: 'Create post by user id error',
+            error: err.message
+        });
+    }
 }
-
+// Delete a post by postId and UserId
 const deletePost = async (req, res) => {
     res.send('DELETE a Post.');
 }
-
 // Exporting functions/methods
 module.exports = {
     getAllPosts,
