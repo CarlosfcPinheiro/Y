d127379f30dcbf543a69b35dace60afb5361e89d
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-11-24 16:57:51 -0300

    alterar request getUser

--- controllers/user.js
+++ controllers/user.js
@@ -28,9 +28,15 @@ const getAllUsers = async (req, res) => {
 const getUser = async (req, res) => {
     const {id} = req.params;
     try{
-        const user = await User.findAll({
+        const user = await User.findOne({
             where: {id:id}
         });
+        // Checks if the user exists
+        if (!user){
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
         res.status(200).json({user});
     } catch (err){
         res.status(500).json({
