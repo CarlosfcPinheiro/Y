786494ca4c13153f2f2cc59bcd3c4db39e7230fa
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-11-27 00:34:27 -0300

    finalizar documentação swagger

--- routes/post.js
+++ routes/post.js
@@ -58,8 +58,15 @@ Router.post('/', authToken);
  *                                  example:
  *                                      name: João
  *          500:
- *              description: Erro ao receber todos os posts.
- * 
+ *              description: Erro ao receber posts
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Error retriving posts
  */
 Router.route('/').get(getAllPosts);
 /**
@@ -91,7 +98,6 @@ Router.route('/').get(getAllPosts);
  *              type: string
  *      responses:
  *          200:
- *              description: Retorna os posts de um usuário específico
  *              content:
  *                  application/json:
  *                      schema:
@@ -123,11 +129,146 @@ Router.route('/').get(getAllPosts);
  *                                  example:
  *                                      name: João
  *          500:
- *              description: Erro ao receber todos os posts do usuário.
+ *              description: Erro ao receber posts por userid
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Error retriving post by userid
  * 
  */
 Router.route('/:userid').get(getPost);
+/**
+ * @swagger
+ * /api/v1/posts:
+ *  post:
+ *      summary: Cria um novo post.
+ *      tags:
+ *          - Posts
+ *      description: Cria um novo post para um usuário já cadastrado.
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      required:
+ *                          - description
+ *                          - userid
+ *                      properties:
+ *                          descricao:
+ *                              type: string
+ *                              description: Mensagem do post do usuário
+ *                              example: Meu novo post
+ *                          img_data:
+ *                              type: string
+ *                              description: Bytea da imagem que foi feita upload
+ *                              example: \x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\n\x00\x00\x00\n\x08\x02\x00\x00\x00\x02PX\xea\x00\x00\x00\x14IDATx\x9ccd`\xf8\xcf\x80\x1b0\xe1\x91\x1b\xc1\xd2\x00?.\x01\x13\\`\xf1\x97\x00\x00\x00\x00IEND\xaeB`\x82
+ *                          userid:
+ *                              type: integer
+ *                              example: 3
+ *      responses:
+ *          201:
+ *              description: Post criado com sucesso
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              id:
+ *                                  type: integer
+ *                                  description: ID do post
+ *                                  example: 1
+ *                              description:
+ *                                  type: string
+ *                                  description: descrição do post
+ *                                  example: meu primeiro post
+ *                              img_data:
+ *                                  type: bytea
+ *                                  description: bytea da imagem vinculada ao post
+ *                                  example: \x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\n\x00\x00\x00\n\x08\x02\x00\x00\x00\x02PX\xea\x00\x00\x00\x14IDATx\x9ccd`\xf8\xcf\x80\x1b0\xe1\x91\x1b\xc1\xd2\x00?.\x01\x13\\`\xf1\x97\x00\x00\x00\x00IEND\xaeB`\x82
+ *                              created_at:
+ *                                  type: data
+ *                                  description: data de criação do post
+ *                                  example: 2024-11-24T23:32:18.700Z
+ *                              userid:
+ *                                  type: integer
+ *                                  description: id do usuário autor do post
+ *                                  example: 2
+ *                              author:
+ *                                  type: object
+ *                                  description: objeto com os dados do usuário autor do post
+ *                                  example:
+ *                                      name: João
+ *          500:
+ *              description: Erro ao criar post.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Create post error
+ */
 Router.route('/').post(postUserPost);
+/**
+ * @swagger
+ * /api/v1/posts/{userid}:
+ *  delete:
+ *      summary: Deleta o Post de um usuário específico
+ *      tags:
+ *          - Posts
+ *      description: Deleta o post de um usuário específico baseado no id
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            description: id do usuário
+ *            schema:
+ *              type: integer
+ *          - in: header
+ *            name: Authorization
+ *            required: true
+ *            description: Token JWT válido para autenticação de usuário
+ *            schema:
+ *              type: string
+ * 
+ *      responses:
+ *          404:
+ *              description: Post não encontrado
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Post not found.
+ *          403:
+ *              description: Ação negada.  
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                 type: string
+ *                                 example: Action denied.  
+ *          500:
+ *              description: Erro ao deletar post.  
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                 type: string
+ *                                 example: Error when delete post. 
+ */
 Router.route('/:id').delete(deletePost);
 
 // Exporting post router
--- routes/user.js
+++ routes/user.js
@@ -41,8 +41,40 @@ Router.patch('/:id', authToken);
  *      responses: 
  *          200:
  *              description: Lista de usuários.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              id:
+ *                                  type: integer
+ *                                  example: 1
+ *                              name:
+ *                                  type: string
+ *                                  example: Joazinho
+ *                              email:
+ *                                  type: string
+ *                                  example: Joaz@gmail.com
+ *                              password:
+ *                                  type: string
+ *                                  example: $2b$05$vIPcflGyrNCbOL8wdWLJEe3d1CFoW3AXhFiRUDR/oCTZQ4Ok02gEe
+ *                              posts_qnt:
+ *                                  type: integer
+ *                                  example: 2
+ *                              created_at:
+ *                                  type: string
+ *                                  example: 2024-11-27T01:07:49.979Z
+ *                              
  *          500: 
  *              description: Erro ao obter usuários.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Error to get users
  */
 Router.route('/').get(getAllUsers);
 
@@ -63,11 +95,50 @@ Router.route('/').get(getAllUsers);
  *              type: integer
  *      responses:
  *          200:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              id:
+ *                                  type: integer
+ *                                  example: 1
+ *                              name:
+ *                                  type: string
+ *                                  example: Joazinho
+ *                              email:
+ *                                  type: string
+ *                                  example: Joaz@gmail.com
+ *                              password:
+ *                                  type: string
+ *                                  example: $2b$05$vIPcflGyrNCbOL8wdWLJEe3d1CFoW3AXhFiRUDR/oCTZQ4Ok02gEe
+ *                              posts_qnt:
+ *                                  type: integer
+ *                                  example: 2
+ *                              created_at:
+ *                                  type: string
+ *                                  example: 2024-11-27T01:07:49.979Z
  *              description: Usuário encontrado.
  *          404:
- *              description: Usuário não encontrado.
+ *              description: Usuário não encontrado pelo ID fornecido
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: User not found
  *          500:
- *              description: Erro ao obter usuário.
+ *              description: Houve um problema ao receber usuário
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Get user by id error
  */
 Router.route('/:id').get(getUser);
 
@@ -104,8 +175,34 @@ Router.route('/:id').get(getUser);
  *                              example: joa123zinho
  *      responses:
  *          201:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: User created successfully
+ *                              User:
+ *                                  type: object
+ *                                  example:
+ *                                      id: 2
+ *                                      name: Joãozinho123
+ *                                      email: joaoz123@gmail.com
+ *                                      password: $2b$05$vIPcflGyrNCbOL8wdWLJEe3d1CFoW3AXhFiRUDR/oCTZQ4Ok02gEe
+ *                                      posts_qnt: 0
+ *                                      created_at: 2024-11-27T01:07:49.979Z                       
  *              description: Usuário criado com sucesso.
  *          500:
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Create user error
+ *                                  
  *              description: Erro ao criar usuário.
  */
 Router.route('/').post(postUser);
@@ -125,25 +222,30 @@ Router.route('/').post(postUser);
  *            description: ID do usuário que está logado e cadastrado no.
  *            schema:
  *              type: integer
+ *          - in: header
+ *            name: Authorization
+ *            required: true
+ *            description: Token JWT válido para autenticação de usuário
+ *            schema:
+ *              type: string
  *      requestBody:
  *          required: false
  *          content:
  *              application/json:
- *                  required:
- *                      - name OU email OU password
- *                  properties:
- *                       name:
- *                          type: string
- *                          description: Nome do usuário
- *                          example: novoJoãozinho123
- *                       email:
- *                          type: string
- *                          description: Email do usuário
- *                          example: novojoaoz123@gmail.com
- *                       password:
- *                          type: string
- *                          description: Senha do usuário
- *                          example: novojoa123zinho
+ *                  schema:
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              description: Nome do usuário
+ *                              example: novoJoãozinho123
+ *                          email:
+ *                              type: string
+ *                              description: Email do usuário
+ *                              example: novojoaoz123@gmail.com
+ *                          password:
+ *                              type: string
+ *                              description: Senha do usuário
+ *                              example: novojoa123zinho
  *      responses:
  *          200:
  *              description: Usuário atualizado com sucesso.
@@ -159,21 +261,21 @@ Router.route('/').post(postUser);
  *                                  type: object
  *                                  example:
  *                                      id: 1
- *                                      name: Joãozinho123
- *                                      email: joaoz123@gmail.com
- *                                      password: jao123zinho
+ *                                      name: Joaozinho123
+ *                                      password: $2b$05$vIPcflGyrNCbOL8wdWLJEe3d1CFoW3AXhFiRUDR/oCTZQ4Ok02gEe
+ *                                      email: Joao@gmail.com
  *                                      posts_qnt: 1
- *                                      created_at: 2024-11-26T22:07:49.979Z
- *                                      
- *                              
+ *                                      created_at: 2024-11-27T01:07:49.979Z
  *          403:
  *              description: Acesso negado.
  *              content:
  *                  application/json:
  *                      schema:
- *                      message:
- *                          type: string
- *                          message: Access denied.
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  message: Access denied.
  *          500:
  *              description: Erro ao atualizar usuário.  
  *              content:
@@ -183,7 +285,7 @@ Router.route('/').post(postUser);
  *                          properties:
  *                              message:
  *                                 type: string
- *                                 message: Update user error.     
+ *                                 example: Update user error.     
  */
 Router.route('/:id').patch(patchUser);
 /**
