94f646adeb099dd9655095ff7aa776eda1acb4ea
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-11-29 22:45:22 -0300

    Finalizar lógica da página de login

--- src/login.js
+++ src/login.js
@@ -2,13 +2,21 @@ const input_username = document.getElementById('nameUser');
 const input_password = document.getElementById('password');
 const button_confirm = document.getElementById('buttonLogin');
 
+// Dynamic elements
+const bg_loading = document.getElementById('loading');
+const login_fail = document.getElementById('login-fail');
+
 const url_login = 'https://prog-webii-projeto.onrender.com/api/v1/users/login';
 
 button_confirm.addEventListener('click', () => {
+    button_confirm.disabled = true;
+
     const username = input_username.value;
     const password = input_password.value;
+
     // Async function to login
     (async () => {
+        bg_loading.classList.add('show');
         try{
             const response = await fetch(url_login, {
                 method: 'POST',
@@ -20,15 +28,33 @@ button_confirm.addEventListener('click', () => {
                     password: password
                 })
             });
-            // Reciving json response
-            const jsonData = await response.json();
-            // Storage authToken on localStorage
-            localStorage.authToken = jsonData.authToken;
-            console.log('Login success');
-            // Change to index page (home)
-            window.location.replace('../index.html');
+            // Check status code
+            const status = checkResponse(response);
+            if (status){
+                console.log("200")
+                // Reciving json response
+                const jsonData = await response.json();
+                // Storage authToken on localStorage
+                localStorage.authToken = jsonData.authToken;
+                // window.location.replace('../index.html');
+                window.location.replace('../index.html');
+            } else {
+                login_fail.classList.add('show');
+            }
         } catch(err) {
             console.log(`Interface login error: ${err}`);
         }
+        // Set default states
+        bg_loading.classList.remove('show');
+        button_confirm.disabled = false;
     })();
 });
+
+// Check request response
+const checkResponse = (responseData) => {
+    if (responseData.status == 200){
+        return true
+    } else {
+        return false
+    }
+}
