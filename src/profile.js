16e02211638931817c14e04b64017d1aab0862ff
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-12-06 10:18:35 -0300

    Adicionar lógica da autenticação : Dinamicidade aos elementos quando já logado

--- src/profile.js
+++ src/profile.js
@@ -1,3 +1,23 @@
+// Checks if the token is still valid
+const isTokenActive = (token) => {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const currentTime = Math.floor(Date.now() / 1000);
+    // Returns true or false
+    return currentTime < payload.exp;
+};
+
+// OBS: TROCAR DE DEFER PARA ASYNC
+  
+const tokenActive = isTokenActive(localStorage.getItem('authToken'));
+// Hide login and register links
+if (tokenActive){
+    const link_login = document.getElementById('login-link');
+    const link_register = document.getElementById('register-link');
+
+    link_login.style.display = 'none';
+    link_register.style.display = 'none';
+}
+
 async function getUser() {
     const idUser = localStorage.id;
     const nameUser = localStorage.name;
